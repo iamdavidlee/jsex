1495ac9df8deb9110c40217345692e5dac9ec7f7
sunghyuk.lee <sunghyuk.lee@ggq.gg>
2024-01-01 13:06:16 +0900

    add async func

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-import { testMakePerson } from "./utils/makePerson";
-testMakePerson();
+import { readFilePromise } from "./utils/ayncFile";
+// readFilePromise("abc.txt")
+// .then(data=>console.log(data))
+// .catch(e => console.log(e));
+
+async function readAbc(filename:string) {
+    console.log("before read");
+    const text = await readFilePromise(filename);
+    console.log("after read");
+    console.log(text);
+}
+
+console.log("before");
+readAbc("abc.txt");
+console.log("after");
